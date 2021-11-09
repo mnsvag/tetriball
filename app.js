916e61669e2cf454c20099d222927debe5c9ca8b
Manasvi Agarwal <manasvi452@gmail.com>
2021-11-09 13:50:54 +0530

    removing color

--- app.js
+++ app.js
@@ -13,21 +13,10 @@ class Block {
     }
 }
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
 function constructBlock(center, type) {
     let newBlock = new Block(center, type)
-    color = getRandomColor();
     blockTypes[type.toString()].forEach(element => {
-        console.log(element);
-        allDivs[center+element].style.backgroundColor = color;
+        allDivs[center+element].style.backgroundColor = "yellow";
     });
 }
 
@@ -35,5 +24,4 @@ initialDiv = Math.floor(Math.random()*10)
 randomType = Math.floor(Math.random()*6)
 constructBlock(initialDiv, randomType)
 
-console.log('check')
 
