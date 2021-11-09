b4be86661365010d2a3817760850d9cf38ac413f
Manasvi Agarwal <manasvi452@gmail.com>
2021-11-09 13:34:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const allDivs = document.querySelectorAll(".pixel");
+const blockTypes = {0: [-1, +9, +10, +11],
+                    1: [+1, +9, +10, +11],
+                    2: [0, +1, +9, +10],
+                    3: [0, +9, +10, +11],
+                    4: [-1, 0, +10, +11],
+                    5: [-1, 0, +1]
+                };
+class Block {
+    constructor(center, type) {
+        this.center = center
+        this.type=type;
+    }
+}
+
+function constructBlock(center, type) {
+    let newBlock = new Block(center, type)
+    blockTypes[type.toString()].forEach(element => {
+        console.log(element);
+        allDivs[center+element].style.backgroundColor="yellow";
+    });
+}
+
+initialDiv = Math.floor(Math.random()*10)
+randomType = Math.floor(Math.random()*6)
+constructBlock(initialDiv, randomType)
+
