e669fbb9490920cffb756e2ba4db1748bce16693
Ashish Prabhune <prabhune.ashish@gmail.com>
2021-11-10 21:30:13 +0530

    Added setInterval & moving down automatically

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const allDivs = document.querySelectorAll(".pixel");
 const rotateButton = document.getElementById("rotateButton")
 const downButton  =  document.getElementById("downButton")
+const leftButton  =  document.getElementById("leftButton")
+const rightButton  =  document.getElementById("rightButton")
+
 
 //map for roatation
 const rotateMap = new Map();
@@ -29,9 +32,36 @@ class Block {
         this.type=type;
         this.color = color;
         this.blockArray = blockTypes[type.toString()]
+        this.isActive= true;
+        this.intervalOfMovingDown = null;
+        this.intervalOfIsActive =  null;
     }
 
-    
+    updateIsActive(){
+        // console.log("i am in update is active")
+        // console.log(this)
+        // console.log(this.blockArray)
+
+        //checks if bottom is touching some div, if yes, updates isActive, else clear interval for checking UpdateIsActive
+        this.blockArray.forEach( ele => {
+            if(allDivs[this.center+ele + 10].style.backgroundColor != "white");
+                //this.isActive = false;
+        })
+
+        if(this.isActive==false){
+            clearInterval(this.intervalOfIsActive);
+        }
+    }
+
+    checkAndMoveDown(){
+        //if isActive is true, go down else clear Interval for going down
+        if(this.isActive==true){
+            this.moveDown();
+        }
+        else
+            clearInterval(this.intervalOfMovingDown);
+
+    }
 
     moveLeft() {
         
@@ -46,9 +76,7 @@ class Block {
             allDivs[this.center+ele].style.backgroundColor="white"
         })
 
-        console.log(this.center);
         this.center += 10;
-        console.log(this.center);
         
 
         this.blockArray.forEach(ele => {
@@ -94,10 +122,30 @@ function constructBlock() {
 //onLoad 
 
 var block;
+// var intervalOfMovingDown;
+// var intervalOfIsActive;
 
 function blockJourney() {
+
+    rotateButton.addEventListener("click", ()=>{
+        block.rotate();
+    });
+
+    downButton.addEventListener("click", ()=>{
+         block.moveDown();
+     });
+
     //1. bring new block
     block = constructBlock()
+
+    block.intervalOfIsActive   = setInterval(function() {block.updateIsActive()}, 10);
+    block.intervalOfMovingDown = setInterval(function() {block.checkAndMoveDown()}, 1000);
+
+    // while(block.isActive){
+
+        
+    // }
+    
     
 
     //2. move the block down
@@ -112,12 +160,12 @@ function blockJourney() {
 
     //bottommost or another block stop movement and call new blockJourney
 
-   rotateButton.addEventListener("click", ()=>{
-       block.rotate();
-   });
-   downButton.addEventListener("click", ()=>{
-        block.moveDown();
-    });
+//    rotateButton.addEventListener("click", ()=>{
+//        block.rotate();
+//    });
+//    downButton.addEventListener("click", ()=>{
+//         block.moveDown();
+//     });
 }
 
 blockJourney();
