94d1099dfe3c7bbf98f6a9f73b42b14768c573e8
Manasvi Agarwal <manasvi452@gmail.com>
2022-04-10 20:47:22 +0530

    restructure code & fix irregular movement bugs

--- app.js
+++ app.js
@@ -65,28 +65,6 @@ class Block {
     }
 
     moveLeft() {
-        // let myLeftMostCols;
-        // for(let col=0;col<=2;col++){
-        //     let found=false;
-        //     for(let row=0;row<3;row++){
-        //         if(this.blockArray.includes(matrix[row][col])){
-        //             found=true;
-        //             break;
-        //         }
-        //     }
-        //     if(found){
-        //         myLeftMostCol=col;
-        //         break;
-        //     }
-        // }
-        
-        // //if leftmost col is border or has some filled div dont move, else move
-        // for(let row=0; row <= 2 ; row++){
-        //     let index = matrix[row][myLeftMostCol];
-        //     if(!this.blockArray.includes(index)) continue
-        //     let leftToLeftMostCol = this.center + index - 1;
-        //     if(isPartOfBorder(leftToLeftMostCol) || isFilled(leftToLeftMostCol)) return;
-        // }
 
         const leftBorder = [];
         for(var row=0; row<3; ++row) {
@@ -110,28 +88,6 @@ class Block {
     }
 
     moveRight() {
-        // let myRightMostCol;
-        // for(let col=2;col >= 0;col--){
-        //     let found=false;
-        //     for(let row=0; row <= 2; row++){
-        //         if(this.blockArray.includes(matrix[row][col])){
-        //             found=true;
-        //             break;
-        //         }
-        //     }
-        //     if(found){
-        //         myRightMostCol=col;
-        //         break;
-        //     }
-        // }
-        
-        // //if leftmost col is border or has some filled div dont move, else move
-        // for(let row=0; row <= 2 ; row++){
-        //     let index = matrix[row][myRightMostCol];
-        //     if(!this.blockArray.includes(index)) continue
-        //     let rightToRightMostCol = this.center + index + 1;
-        //     if(isPartOfBorder(rightToRightMostCol) || isFilled(rightToRightMostCol)) return;
-        // }
 
         const rightBorder = [];
         for(var row=0; row<3; ++row) {
@@ -156,38 +112,6 @@ class Block {
     }
 
     moveDown() {
-
-        // let myDownMostRow;
-        // for(let row=2;row >= 0;row--){
-        //     let found=false;
-        //     for(let col=0; col <= 2; col++){
-        //         if(this.blockArray.includes(matrix[row][col])){
-        //             found=true;
-        //             break;
-        //         }
-        //     }
-        //     if(found){
-        //         myDownMostRow=row;
-        //         break;
-        //     }
-        // }
-        
-        // //if leftmost col is border or has some filled div dont move, else move
-        // for(let col=0; col <= 2 ; col++){
-        //     let index = matrix[myDownMostRow][col];
-        //     if(!this.blockArray.includes(index)) continue
-        //     let downToDownMostCol = this.center + index + 12;
-        //     if(isPartOfBorder(downToDownMostCol) || isFilled(downToDownMostCol)) {
-                // newBlockFlag=true
-                // this.blockArray.forEach(ele => {
-                //     allDivs[this.center+ele].classList.remove("moving")
-                //     allDivs[this.center+ele].classList.add("block")
-                // })
-                // clearInterval(this.intervalOfMovingDown)
-                // document.removeEventListener('keydown', keyBoard)
-        //         return
-        //     }
-        // }
 
         const downBorder = [];
         for(var col=0; col<3; ++col) {
@@ -271,14 +195,6 @@ var block;
 
 function blockJourney() {
 
-    // rotateButton.addEventListener("click", ()=>{
-    //     block.rotate();
-    // });
-
-    // downButton.addEventListener("click", ()=>{
-    //      block.moveDown();
-    //  });
-
     //1. bring new block
     block = constructBlock()
 
@@ -286,10 +202,6 @@ function blockJourney() {
     block.intervalOfMovingDown = setInterval(function() {block.moveDown()}, 1000);
 
     document.addEventListener('keydown', keyBoard);
- 
-//    while(!newBlockFlag) {console.log("stuck")}
-
-//     document.removeEventListener('keydown', keyBoard)
 }
 
 //blockJourney();
@@ -320,154 +232,91 @@ gameInterval =  setInterval(()=> {
 //     console.log("huh")
 //     clearInterval(gameInterval)
 // }
-/*
 
-Code/game structure
-checkAndMoveDown(){
-     if(block is currently active && can go down){
-         moveDown();
-     }
-     else clearInterval(checkAndMoveDown)
+
+
+        
+document.addEventListener('mousemove', plankMovement);
+
+//update canvas 30 times in 1s
+const FPS = 50;
+
+let current_y = canvas.height/2;
+let current_x = canvas.width/2;
+let ball_size = 15;
+let speed =10;
+let theta=Math.PI/3;
+let cos_theta, sin_theta;
+let plank_start = 0;
+let plank_length=100;
+let ctx = document.getElementById('canvas').getContext('2d');
+calculateAngles();
+
+
+setInterval(draw, 1000 / FPS);
+
+function calculateAngles() {
+    cos_theta = Math.cos(theta);
+    sin_theta = Math.sin(theta);
 }
 
-updateIsActive(){
-    checkIf touching down
-    else
-        clearInterval(updateIsActive)
+function plankMovement(e) {
+    plank_start = e.offsetX;
 }
 
-function keyBoard(block){
-    if user presses left go left ==> block.moveLeft()
-    if user presses right go right
-    if user presses space rotate
+function isPartOfPlank (x,y) {
+    if(x  >= plank_start 
+        && x <= plank_start+plank_length 
+        && y >= canvas.height-30) 
+            return true;
+    else return false;
 }
 
-blockjourney(){
+function updateBallPosition() {
+    current_y  = current_y +  speed*sin_theta;
+    current_x  = current_x +  speed*cos_theta;
+}
 
-    createBlock
+function checkForCollision() {
+    let changed=false;
 
-    setInterval(updateIsActive,100ms); //this will keep updating if the block is active
-    setInterval( checkAndMoveDown, 1 second); //this will keep checking if the block is active and then move down every second
-    
-    document.addEventListener() ("keypress",keyBoard(block))
-    while block.isActive{
-       ;
+    if(isPartOfPlank(current_x,current_y)) {
+        theta = 2*Math.PI-theta;
+        changed=true;
     }
-    document.removeEventListner()
+
+    if (current_y - ball_size/2 <= 0) {
+            theta = 2*Math.PI-theta;
+            changed=true;
+    }
+
+    if (current_x - ball_size/2 <= 0
+        || current_x + ball_size/2 >= canvas.width) {
+            theta = Math.PI-theta;
+            changed= true;
+    }
+
+    calculateAngles();
+
+    return changed;
 }
 
-while(game continues){
-    blockJourney();
+function draw() {
+
+    updateBallPosition();
+
+    if(checkForCollision()) {
+       updateBallPosition();
+    }  
+
+    ctx.clearRect(0, 0, 720, 510); // clear canvas  
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(plank_start, canvas.height-30 , plank_length , 20);
+    
+    ctx.beginPath();
+    ctx.arc(current_x, current_y, ball_size, 0, 2*Math.PI);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+    ctx.closePath();
 }
-*/
-
-var ball = new Image();
-        
-      document.addEventListener('mousemove', plankMovement);
-
-      let current_y = 480;
-      let current_x = 30;
-      let current_time = Date.now();
-      let speed =2.25;
-      let angle=60;
-      let theta,cos_theta, sin_theta;
-      let plank_start = 30,plank_length=50;
-      recalculateValues();
-
-      function init(){
-          ball.src = "https://cdn.britannica.com/68/195168-050-BBAE019A/football.jpg"
-          let value = 20;
-          let current_time = Date.now();
-          window.requestAnimationFrame(draw);
-        }
-
-      function recalculateValues(){
-        theta = angle*(Math.PI/180.0);
-        cos_theta = Math.cos(theta);
-        sin_theta = Math.sin(theta);
-      }
-
-      function plankMovement(e){
-        plank_start = e.offsetX;
-        
-      }
-      function isPartOfPlank(x,y){
-        
-        if(x >= plank_start && x <= plank_start+plank_length && y>= 480) return true;
-        else return false;
-      }
-
-      function positionIsNotValid(x,y){
-        if(x <= 30 || x >= 330) return true;
-        if(y <= 30 || y >= 480) return true;
-        return false;
-      }
-
-      function draw() {
-        
-		    var ctx = document.getElementById('canvas').getContext('2d');
-
-        let cur = Date.now();
-        let delta = cur - current_time;
-        delta/= 25;
-
-
-      
-          current_y  = current_y + delta* speed*sin_theta;
-          current_x  = current_x + delta* speed*cos_theta;
-
-          let changed=false;
-
-          if(isPartOfPlank(current_x,current_y))  {angle = 360-angle; changed=true;}
-          if(current_y <= 30) {angle = 360-angle; changed=true;}
-          if(current_x >= 330 || current_x <= 30) {angle = 180-angle; changed= true;}
-
-          //console.log(val);
-
-          recalculateValues();
-
-          if(changed){
-            current_y  = current_y + delta* speed*sin_theta;
-            current_x  = current_x + delta* speed*cos_theta;
-
-            current_y  = current_y + delta* speed*sin_theta;
-            current_x  = current_x + delta* speed*cos_theta;
-          }
-
-          
-        
-
-        console.log(current_x,current_y);
-
-        // if(val = isPartOfPlank(current_x,current_y)) angle = 360-angle;
-        // //if(current_y >= 300) current_y = 500;
-
-        // console.log(val);
-
-        // //if(current_y >= 280 || current_y <= 10) angle = 360-angle;
-        // if( current_y <= 10) angle = 360-angle;
-        // if(current_x >= 280 || current_x <= 10) angle = 180-angle;
-
-        // if(angle < 0 ) angle += 360;
-        // if(angle >= 360) angle -= 360;
-
-        // recalculateValues();
-
-        // current_y  = current_y + delta* speed*sin_theta;
-        // current_x  = current_x + delta* speed*cos_theta;
-
-        
-
-        
-
-        current_time = cur;     
-        
-        ctx.clearRect(0, 0, 360, 510); // clear canvas  
-        ctx.drawImage(ball, current_x, current_y,30,30);
-
-        ctx.fillRect(plank_start,480,100,20);
-        window.requestAnimationFrame(draw);
-
-  
-        
-      }
